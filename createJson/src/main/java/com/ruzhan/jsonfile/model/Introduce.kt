f889f3e9460432d45d0af8494a03e961d026906d
ruzhan123 <dev19921116@gmail.com>
2019-12-03 20:08:50 +0800

    update gradle config

---
+++ createJson/src/main/java/com/ruzhan/jsonfile/model/Introduce.kt
@@ -0,0 +1,10 @@
+package com.ruzhan.jsonfile.model
+
+class Introduce(var type: Int, var text: String, var image: String) {
+
+    companion object {
+
+        const val TEXT = 1000
+        const val IMAGE = 1001
+    }
+}
